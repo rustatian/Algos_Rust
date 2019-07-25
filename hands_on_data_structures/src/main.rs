df0fbcf325712087bd1a1d7b33fdc9318cc553e3
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-07-25 23:04:57 +0300

    A long time ago, in a galaxy far far away...

---
+++ hands_on_data_structures/src/main.rs
@@ -0,0 +1,29 @@
+use std::rc::Rc;
+
+fn main() {
+    no_ref_counter();
+}
+
+#[allow(dead_code)]
+struct MyStruct {
+    x: Vec<i32>,
+}
+
+#[derive(Debug)]
+struct FileName {
+    name: Rc<String>,
+    ext: Rc<String>
+}
+
+
+fn no_ref_counter() {
+    let name = Rc::new(String::from("main"));
+    let ext = Rc::new(String::from("rs"));
+
+    for _ in 0..3 {
+        println!("{:?}", FileName {
+            name:name.clone(),
+            ext:ext.clone(),
+        })
+    }
+}
