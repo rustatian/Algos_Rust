16ed5a2db8af5545f1acc36ab342a1ee15869cd3
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-07-29 12:02:31 +0300

    Ok

---
+++ CodeWars/7kyu/mumbling/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+// Rust test example:
+// TODO: replace with your own tests (TDD), these are just how-to examples.
+// See: https://doc.rust-lang.org/book/testing.html
+
+#[test]
+fn basic_tests() {
+    assert_eq!(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+    assert_eq!(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+    assert_eq!(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+    assert_eq!(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+    assert_eq!(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+}
+
+fn accum(s: &str) -> String {
+    return s.chars().fold("", |i, c| {
+        if !c.is_uppercase() {
+            c.to_uppercase();
+        };
+
+        println!("{}", c);
+        return s
+
+    }).to_string()
+}
