17df0b847474fcc45600eb421615b84682363554
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-09-24 00:24:21 +0300

    minor reorganization

--- Books/hands_on_data_structures/src/main.rs
+++ Books/hands_on_data_structures/src/main.rs
@@ -1,7 +1,7 @@
-use crate::list::*;
+mod lists;
 
 fn main() {
-    let mut tl = TransactionLog::new_empty();
+    let mut tl = lists::linked_list::TransactionLog::new_empty();
 
     tl.append(String::from("some_value"));
     tl.append(String::from("some_value_2"));
