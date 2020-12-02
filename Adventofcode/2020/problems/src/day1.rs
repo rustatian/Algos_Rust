c753ece53d7ddb3b1cff966eda137e3ae8aec460
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-02 12:34:20 +0300

    Day1 part2 solved

--- Adventofcode/2020/problems/src/day1.rs
+++ Adventofcode/2020/problems/src/day1.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use std::cmp::Ordering;
     use std::collections::HashMap;
     use std::fs::File;
     use std::io::Read;
@@ -9,12 +10,12 @@ mod tests {
         let mut f = File::open("data/day1.txt").unwrap();
         let mut s = String::new();
         let _ = f.read_to_string(&mut s);
-        let splitted: Vec<u32> = s.split('\n').map(|x| x.parse::<u32>().unwrap()).collect();
+        let mut splitted: Vec<u32> = s.split('\n').map(|x| x.parse::<u32>().unwrap()).collect();
 
         let mut hm: HashMap<u32, bool> = HashMap::new();
 
         let year = 2020;
-        for num in splitted {
+        for num in &splitted {
             let diff = year - num;
             if hm.contains_key(&num) {
                 println!("the result is: {}", num * diff);
@@ -24,6 +25,33 @@ mod tests {
             }
         }
 
-        println!("done");
+        println!("done part 1");
+        println!("part 2 -------");
+
+        splitted.sort_unstable();
+
+        for (i, _) in splitted.iter().enumerate() {
+            let mut left = i + 1;
+            let mut right = splitted.len() - 1;
+
+            while left < right {
+                let curr = splitted[i] + splitted[right] + splitted[left];
+                match curr.cmp(&year) {
+                    Ordering::Greater => {
+                        right -= 1;
+                    }
+                    Ordering::Less => {
+                        left += 1;
+                    }
+                    Ordering::Equal => {
+                        println!(
+                            "the part2 result is: {}",
+                            splitted[i] * splitted[right] * splitted[left]
+                        );
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
