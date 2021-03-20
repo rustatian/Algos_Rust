118acbe2f90ddf1ad812bc1ef867c5d2580c55c7
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-03-20 23:48:19 +0300

    Update caesar_cipher_encryption

--- Algoexpert/easy/src/caesar_cipher_encryption.rs
+++ Algoexpert/easy/src/caesar_cipher_encryption.rs
@@ -1,22 +1,28 @@
-use std::borrow::BorrowMut;
-
 struct CaesarCipherEncryption {}
 
 impl CaesarCipherEncryption {
     fn decrypt(data: &mut String, key: i32) -> String {
         let shift = key % 26;
         let offset = 26;
-        let mut target = data.borrow_mut().as_bytes();
+        let mut target = data.clone().into_bytes();
 
-        for i in 0..target.len() {
-            let mut a = b"";
+        let len = target.len();
+
+        let mut i = 0;
+
+        loop {
+            if i >= len {
+                break;
+            }
             if target[i] >= b'a' && target[i] <= b'z' {
-                target[i] += 2 as u8;
-            } else {}
+                target[i] += shift as u8;
+            } else {
+                target[i] = target[i] + shift as u8 - offset;
+            }
+            i += 1;
         }
 
-
-        "fasd".to_string()
+        target.iter_mut().map(|x| *x as char).collect::<String>()
     }
 }
 
@@ -26,6 +32,9 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(CaesarCipherEncryption::decrypt(&mut "xyz".to_string(), 2), "zab".to_string());
+        assert_eq!(
+            CaesarCipherEncryption::decrypt(&mut "xyz".to_string(), 2),
+            "zab".to_string()
+        );
     }
 }
